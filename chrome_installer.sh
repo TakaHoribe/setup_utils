0f116b5d8b65e3ff1abc1bedaf35d3b01831e7a6
Takamasa Horibe <horibe.takamasa@gmail.com>
2020-12-07 01:45:23 +0900

    update
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

---
+++ chrome_installer.sh
@@ -0,0 +1,4 @@
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt update
+sudo apt-get install google-chrome-stable
