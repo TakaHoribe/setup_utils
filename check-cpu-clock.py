6992d93226b2420397bf5d430c6b384e7e1c3f10
Takamasa Horibe <horibe.takamasa@gmail.com>
2022-03-11 18:54:54 +0900

    add check-cpu-clock.py
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

---
+++ check-cpu-clock.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python$
+
+import os
+import subprocess
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+t_arr = []
+v_arr = []
+
+t = 0.0
+dt = 1.0
+
+plt.ion()
+figure, ax = plt.subplots(figsize=(8,6))
+line1, = ax.plot(t_arr, v_arr)
+
+plt.xlabel("time [s]",fontsize=10)
+plt.ylabel("cpu freq average",fontsize=10)
+try:
+    while True:
+        res = subprocess.check_output('cat /proc/cpuinfo | grep MHz | cut -d " " -f 3', shell=True, text=True)
+        res2 = res.split('\n')[0:12]
+        val = [float(item) for item in res2]
+        ave = sum(val) / len(val)
+
+        time.sleep(dt)
+
+        t_arr.append(t)
+        v_arr.append(ave)
+
+        t = t + dt
+
+        line1.set_data(t_arr, v_arr)
+        ax.set_xlim((0, t_arr[-1]))
+        ax.set_ylim((1000, 3500))
+
+        plt.pause(.01)
+except KeyboardInterrupt:
+    print('stop!')
