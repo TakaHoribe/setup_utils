207e3ce6f853d9bc9dc8bc59ef4f435ea618c37c
Takamasa Horibe <horibe.takamasa@gmail.com>
2023-01-02 12:00:31 +0900

    update
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

--- install_utils.sh
+++ install_utils.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt install git openssh-server terminator silversearcher-ag axel
+sudo apt install git openssh-server terminator silversearcher-ag axel simplescreenrecorder
