77885859dc20cd8f8c98beedd143f6c27e7de0b1
Takamasa Horibe <horibe.takamasa@gmail.com>
2023-01-02 11:58:31 +0900

    update
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

---
+++ all.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+install_utils.sh
+
+chrome_installer.sh
+
+vscode_installer.sh
+
+setup_bashrc.sh
---
+++ install_utils.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install git openssh-server terminator silversearcher-ag axel
