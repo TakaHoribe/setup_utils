4a267f8ca1353156a1755dfdea266a71db14dd6a
Takamasa Horibe <horibe.takamasa@gmail.com>
2023-05-16 13:45:42 +0900

    update
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

--- setup_bashrc.sh
+++ setup_bashrc.sh
@@ -2,3 +2,4 @@ cat ./bashrc_custom/alias.txt >> ~/.bashrc
 cat ./bashrc_custom/ccache.txt >> ~/.bashrc
 cat ./bashrc_custom/display_git_branch.txt >> ~/.bashrc
 cat ./bashrc_custom/ros_setup.txt >> ~/.bashrc
+cat ./bashrc_custom/enable_core_dump.txt >> ~/.bashrc
